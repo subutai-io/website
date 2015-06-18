28b7aa30914babda754a1c9d57fb6505f59e90f8
istus <istus@mail.critical-factor.com>
2015-06-18 18:07:24 +0600

    missing search.png fix, + analytics

---
+++ js/plugins/analytics.js
@@ -0,0 +1,34 @@
+function sendData() {
+    var path = "http://subutai.io:1234";
+    var params = "referrer=" + document.referrer +
+                "&agent=" + navigator.userAgent +
+                "&w=" + screen.width +
+                "&h=" + screen.height;
+
+    var e11fg;
+
+    if ( window.XDomainRequest ) {
+        var xdr = new XDomainRequest();
+        xdr.open( "POST", path );
+        xdr.withCredentials = true;
+        xdr.onload = function () {
+            e11fg = parseInt(xdr.responseText);
+        };
+        xdr.send(params);
+    }
+    else if ( window.XMLHttpRequest ) {
+        var xhr = new XMLHttpRequest();
+
+        xhr.open("POST", path, true);
+        xhr.withCredentials = true;
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4) {
+                e11fg = parseInt(xhr.responseText);
+            }
+        };
+        xhr.send(params);
+    }
+}
+
+sendData();
