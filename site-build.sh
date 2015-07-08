268ec8775cccb9a1e51e97e68b31ef7a19947eca
istus <istus@mail.critical-factor.com>
2015-07-08 11:34:23 +0600

    git bin revert image modifications, + site-build , they are incomplete yet

---
+++ site-build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pushd ../project-descriptors
+  git checkout master && git pull
+popd
+git checkout master && git pull
+git bin --init=s3://subutai-website
+git bin --sync
+bash import_content.sh
