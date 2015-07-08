916261e63cbae009f98d4426a43ef85c8e6ac65f
Zubaidullo Niimatullo uulu <zniimatullo@critical-factor.com>
2015-07-08 15:54:43 +0600

    added git commits

--- import_content.sh
+++ import_content.sh
@@ -61,6 +61,30 @@ for descriptor in `find $projects_dir -type f -regex '.*\.json'`; do
   project_name=$(node -pe 'JSON.parse(process.argv[1]).name' "$(cat $projects_dir/$key.json)")
   url=$(node -pe 'JSON.parse(process.argv[1]).website.website' "$(cat $projects_dir/$key.json)")
   parent=$(node -pe 'JSON.parse(process.argv[1]).parent' "$(cat $projects_dir/$key.json)")
+  commits=$(curl -u dashbot:ghkf346LU538QZRD -X GET 'https://stash.subutai.io/rest/api/1.0/projects/SSFSITE/repos/main/commits/?until=master' -A 'ssf')
+  commits=$(node -pe '
+            var commits=[];
+            var commit = {};
+            if ('"$commits"'.values){
+                for (var j=0; j<'"$commits"'.values.length; j++){
+                var cmt={};
+                cmt.message='"$commits"'.values[j].message;
+                cmt.author='"$commits"'.values[j].author.name;
+                cmt.displayId='"$commits"'.values[j].displayId;
+                commits.push(cmt);
+                }
+                commit.commits=commits;
+                JSON.stringify(commit);
+            }
+            else {
+                commit.commits=[];
+                JSON.stringify(commit);
+            }
+           ')
+  echo $commits > $DESCR_PATH/commits.json
+  commits=$(node_modules/.bin/json2yaml "$DESCR_PATH"/commits.json)
+  commits=${commits:4}
+
   blog=$(curl -u dashbot:ghkf346LU538QZRD -X GET 'https://confluence.subutai.io/rest/api/content?type=blogpost&spaceKey='$key'' -A 'ssf')
   blogs=$(node -pe '
                 var blogs=[];
@@ -95,6 +119,7 @@ for descriptor in `find $projects_dir -type f -regex '.*\.json'`; do
   sed -i 's/categories/tags/g' $projects_dir/$now-$key.markdown
   cat << EOF >> $projects_dir/$now-$key.markdown
 
+$commits
 $blogs
   parenturl: $parent
   layout: post
