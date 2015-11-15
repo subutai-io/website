3287c887e2bf3b5b64c49ea347fbefa2783d5c75
daralbaev <daralbaev@critical-factor.com>
2015-11-15 19:52:59 +0600

    SSFSITE-253 tried to update nodejs to v5 with no luck staying on v0.10.25

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,8 +5,6 @@ apt-get -y -qq update
 apt-get -y install software-properties-common
 #add repository for ruby2
 apt-add-repository -y ppa:brightbox/ruby-ng
-
-#get 2.2 from newly added repository
 apt-get -y -qq update
 
 #install ruby with build tools so we can use gems
@@ -16,12 +14,14 @@ apt-get -y install ruby2.2 ruby2.2-dev
 apt-get -y install nodejs-legacy npm
 
 #install requirements (jekyll)
+echo "Installing jekyll"
 gem update --system
 echo "gem: --no-ri --no-rdoc --no-document" > ~/.gemrc
 gem install bundler
 gem install jekyll -v 2.5.3
 
-curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+echo "Configuring aws command line utilities"
+curl -s "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
 ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 rm awscli-bundle.zip
