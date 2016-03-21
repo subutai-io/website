765880c05833aebee01d1e0c313a8edcff8d9f15
Manas Atabaev <matabaev@optimal-dynamics.com>
2016-03-21 10:50:54 +0600

    add new pages

--- ss-website/js/main.js
+++ ss-website/js/main.js
@@ -111,20 +111,6 @@ $(document).ready(function() {
 	
 	
 	
-	$('.js-link-sroll a[href^="#"]').on('click',function (event) {
-		event.preventDefault();
-		var target = this.hash;
-		var $target = $(target);
-	
-		$('html, body').stop().animate({
-			'scrollTop': $target.offset().top
-		}, 900, 'swing', function () {
-			window.location.hash = target;
-		});
-	});
-	
-	
-	
 	// Remodal init
 	$('[data-remodal-id]').remodal();
 	
