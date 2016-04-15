bcbb0401c66fc28380cce0e2209c946f63c1f604
Manas Atabaev <matabaev@optimal-dynamics.com>
2016-04-15 17:08:02 +0600

    dev mac url

---
+++ ss-website/source/source/modules/sidebar-menu/sidebar-menu.js
@@ -0,0 +1,15 @@
+/*$(document).on('click', '.sidebar-menu__link', function () {
+	if ($(this).next('.sidebar-menu__submenu').length > 0) {
+		if ($(this).parent().hasClass('sidebar-menu__item_active')) {
+			$(this).parent().removeClass('sidebar-menu__item_active');
+			$(this).next('.sidebar-menu__submenu').stop().slideUp(300);
+		} else {
+			$('.sidebar-menu__item_active .sidebar-menu__submenu').parent().removeClass('sidebar-menu__item_active')
+			$('.sidebar-menu__submenu').stop().slideUp(300);
+			$(this).parent().addClass('sidebar-menu__item_active');
+			$(this).next('.sidebar-menu__submenu').stop().slideDown(300);
+		}
+		return false;
+	}
+});*/
+
