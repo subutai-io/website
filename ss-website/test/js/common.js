366e5a189fadf50465f8706491c074f39b1bb597
emilov <eemilov@optimal-dynamics.com>
2017-10-17 17:38:24 +0600

    1.0

--- ss-website/test/js/common.js
+++ ss-website/test/js/common.js
@@ -3,5 +3,13 @@ jQuery.noConflict()(function(){
 	jQuery(".phone_bars-burger").on("click", function () {
 		jQuery(".phone").slideToggle(500);
 	});
-	
+	var $grid = $('.gallery-wrapper').masonry({
+		columnWidth: '.grid-sizer',
+		percentPosition: true,
+		transitionDuration: 0,
+	});
+
+	$grid.imagesLoaded().progress( function() {
+		$grid.masonry();
+	});
 });
